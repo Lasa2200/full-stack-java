4f80ab7317e145b81fc906696db42007851b49a7
Lasa2200 <lisbeths.a@gmail.com>
2020-12-28 10:59:58 -0300

    java

---
+++ JAVA/JavaFun/HolaMundo2.java
@@ -0,0 +1,7 @@
+public class HolaMundo2 {
+    public static void main(String[] args) {
+        System.out.println("lisbeth");
+        System.out.println("43");
+        System.out.println("Temuco");
+    }
+}
---
+++ JAVA/MiPrimerPrograma/src/com/talento/java/projecto1/informacion.java
@@ -0,0 +1,15 @@
+package com.talento.java.projecto1;
+
+public class informacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int edad=43;
+		String nombre="Lisbeth";
+		String ciudad="Temuco";
+		System.out.println("mi nombre es "+ nombre);
+		System.out.println("tengo "  + edad + " años de edad");
+		System.out.println("Mi ciudad es "+ ciudad);
+	}
+
+}
